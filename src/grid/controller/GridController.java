3c7210346d6ac11551e61e7c8d2dcd7a32f67dd8
kmart22 <kmar7070@740s-pl-111.local>
2017-02-01 08:49:47 -0700

    added all my gui components and and grid

--- src/grid/controller/GridController.java
+++ src/grid/controller/GridController.java
@@ -6,11 +6,13 @@ import grid.view.GridFrame;
 public class GridController
 {
 	private GridFrame appFrame;
+	private Number [][] grid;
 	
 	public GridController()
 	{
 		// create model objects here!
 		
+		grid = new Number[6][5];
 		appFrame = new GridFrame(this);
 		
 	}
@@ -18,5 +20,15 @@ public class GridController
 	public void start()
 	{
 		JOptionPane.showMessageDialog(appFrame, " hey look at my 2d creation!");
+	}
+	
+	public Number[][] getGrid()
+	{
+		return grid;
+	}
+	
+	public GridFrame getFrame()
+	{
+		return appFrame;
 	}
 }
--- src/grid/view/GridPanel.java
+++ src/grid/view/GridPanel.java
@@ -1,37 +1,67 @@
 package grid.view;
 import javax.swing.*;
+import javax.swing.table.*;
+import java.awt.Dimension;
+import java.awt.Color;
 import grid.controller.GridController;
 
 
 public class GridPanel extends JPanel
 {
 	private JTextField rowField;
-	private JTextField columnField;
+	private JTextField colField;
 	private JTextField inputField;
 	private JLabel inputLabel;
 	private JLabel rowLabel;
-	private JLabel columnLabel;
+	private JLabel colLabel;
 	private JButton submitButton;
+	private JTable gridTable;
+	private JScrollPane gridPane;
+	
 	private SpringLayout baseLayout;
 	private GridController baseController;
+	
 	
 	public GridPanel(GridController baseController)
 	{
 		super();
-		this.baseController = baseController;
+		
+		this.rowField = new JTextField("rowField");
+		this.colField = new JTextField("colField");
+		this.inputField = new JTextField("inputField");
+		this.inputLabel = new JLabel("inputLabel");
+		this.rowLabel = new JLabel("rowLabel");
+		this.colLabel = new JLabel("colLabel");
+		this.submitButton = new JButton("sumbitButton");
+		this.gridTable = new JTable();
+		
 		this.baseLayout = new SpringLayout();
-		//this.rowField = new JTextField 
+		this.baseController = baseController;
 		
-		
-		
-		
-		
+		setupTable();
+		setupPanel();
+		setupLayout();
+		setupListeners();	
 		
 	}
  	
+	private void setupTable()
+	{
+		//load model
+		DefaultTableModel data = new DefaultTableModel(baseController.getGrid(), new String []{"one", "two", "three", "four", "five"});
+		gridTable = new JTable();
+		gridTable.setModel(data);
+		gridPane = new JScrollPane();
+		gridPane.setViewportView(gridTable);
+	}
+	
 	private void setupPanel()
 	{
-		
+		this.setLayout(baseLayout);
+		this.setMinimumSize(new Dimension(700, 700));
+		this.setBackground(Color.PINK);
+		this.add(gridPane);
+		this.add(colField);
 	}
 	
 	private void setupLayout()
@@ -44,4 +74,8 @@ public class GridPanel extends JPanel
 		
 	}
 
+	public GridController getBaseController()
+	{
+		return baseController;
+	}
 }
