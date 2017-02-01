7bfd5556fa297ed8023b2f5ef03b964ee1473b5c
kmart22 <kmar7070@740s-pl-111.local>
2017-02-01 09:20:35 -0700

    made all my components actually show up on the window builder

--- src/grid/view/GridPanel.java
+++ src/grid/view/GridPanel.java
@@ -27,7 +27,6 @@ public class GridPanel extends JPanel
 		super();
 		
 		this.rowField = new JTextField("rowField");
-		this.colField = new JTextField("colField");
 		this.inputField = new JTextField("inputField");
 		this.inputLabel = new JLabel("inputLabel");
 		this.rowLabel = new JLabel("rowLabel");
@@ -36,6 +35,7 @@ public class GridPanel extends JPanel
 		this.gridTable = new JTable();
 		
 		this.baseLayout = new SpringLayout();
+		
 		this.baseController = baseController;
 		
 		setupTable();
@@ -52,6 +52,7 @@ public class GridPanel extends JPanel
 		gridTable = new JTable();
 		gridTable.setModel(data);
 		gridPane = new JScrollPane();
+		
 		gridPane.setViewportView(gridTable);
 	}
 	
@@ -61,12 +62,39 @@ public class GridPanel extends JPanel
 		this.setMinimumSize(new Dimension(700, 700));
 		this.setBackground(Color.PINK);
 		this.add(gridPane);
-		this.add(colField);
+		
+		this.add(rowField);
+		this.add(inputField);
+		this.add(inputLabel);
+		this.add(rowLabel);
+		this.add(colLabel);
+		this.add(submitButton);
+		this.colField = new JTextField("colField");
+		
+		add(colField);
+		
 	}
 	
 	private void setupLayout()
 	{
-		
+		baseLayout.putConstraint(SpringLayout.NORTH, inputLabel, 0, SpringLayout.NORTH, colLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, inputLabel, 0, SpringLayout.WEST, inputField);
+		baseLayout.putConstraint(SpringLayout.NORTH, colLabel, 14, SpringLayout.SOUTH, rowLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, inputField, 41, SpringLayout.EAST, rowLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, colLabel, 0, SpringLayout.WEST, rowLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, rowLabel, 5, SpringLayout.NORTH, rowField);
+		baseLayout.putConstraint(SpringLayout.WEST, rowLabel, 6, SpringLayout.EAST, rowField);
+		baseLayout.putConstraint(SpringLayout.SOUTH, submitButton, 0, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, submitButton, 0, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, inputField, 6, SpringLayout.SOUTH, gridPane);
+		baseLayout.putConstraint(SpringLayout.NORTH, rowField, 6, SpringLayout.SOUTH, gridPane);
+		baseLayout.putConstraint(SpringLayout.NORTH, gridPane, 0, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, gridPane, 0, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, gridPane, -85, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, colField, 4, SpringLayout.SOUTH, rowField);
+		baseLayout.putConstraint(SpringLayout.WEST, colField, 23, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, rowField, 0, SpringLayout.WEST, colField);
+		baseLayout.putConstraint(SpringLayout.EAST, rowField, 63, SpringLayout.WEST, colField);
 	}
 	
 	private void setupListeners()
